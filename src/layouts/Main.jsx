c8e559f108d761cce8e1000948458d25cdf028af
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-21 20:21:42 +0600

    footer has been designed

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const Main = () => {
     return (
         <div>
             <h3>This is the Main layout.</h3>
+            <Button variant="warning">Warning</Button>
         </div>
     );
 };
