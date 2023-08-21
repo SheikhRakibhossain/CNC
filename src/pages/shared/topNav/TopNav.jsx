0821bac0b74f2a84cb8e4f7e8989215be7719128
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-21 21:57:05 +0600

    marquee and moment time added

--- src/pages/shared/topNav/TopNav.jsx
+++ src/pages/shared/topNav/TopNav.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
+
 
 const TopNav = () => {
     return (
         <>
-        <Image src="/src/assets/top heading.png" fluid />
-        
+            <div>
+                <Image src="/src/assets/top heading.png" fluid />
+            </div>
+            <div className='py-4'>
+                 <Marquee>
+                    <h4 className='fs-4 mx-5'> "Zelensky warns Russia's neighbors under threat 'if Ukraine does not prevail"</h4>
+                    <h4 className='fs-4 mx-4'> "Hilary moves through US Southwest with ‘historic amount of rainfall !"</h4>
+                    <h4 className='fs-4 mx-4'> "TV chef says he’s closing his Michelin-starred restaurant for ‘better work/life balance’ "</h4>
+                </Marquee>
+            </div>
+
         </>
     );
 };
