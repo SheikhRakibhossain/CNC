392368d424eb0835929d4750756954098a3d52f8
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-21 19:56:12 +0600

    main and home page layout created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h3>Hello World</h3>
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h3>This is the Main layout.</h3>
+        </div>
+    );
+};
+
+export default Main;
