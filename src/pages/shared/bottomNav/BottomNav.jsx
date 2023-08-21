dd66cd5e51b05f252183eeb7260039d7d3426f96
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-22 01:38:07 +0600

    navbar has been designed

--- src/pages/shared/bottomNav/BottomNav.jsx
+++ src/pages/shared/bottomNav/BottomNav.jsx
@@ -1,35 +1,82 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
 import { Link } from 'react-router-dom';
-import './BottomNav.css'
+import './BottomNav.css';
+import { FaBeer, FaSearch, FaUser } from 'react-icons/fa';
 
 const BottomNav = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <Link class="navbar-brand" href="#"><img src={logo} alt="logo" className='logo' /></Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+        <nav class="navbar navbar-expand-lg bg-dark">
+            <div class="container-fluid">
+                <Link class="navbar-brand" href="#"><img src={logo} alt="logo" className='logo' /></Link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarTogglerDemo02">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 fw-semibold text-white">
+                        <li class="nav-item">
+                            <a class="nav-link text-white active" aria-current="page" href="#">US</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">World</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white " aria-disabled="true">Politics</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" aria-disabled="true">Business</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" aria-disabled="true">Opinions</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white " aria-disabled="true">Health</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" aria-disabled="true">Entertainment</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" aria-disabled="true">Style</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" aria-disabled="true">Travel</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" aria-disabled="true">Sports</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" aria-disabled="true">Video</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" aria-disabled="true">Research</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" aria-disabled="true">Education</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" aria-disabled="true">Foods</a>
+                        </li>
+                    </ul>
+                    {/* Right nav bar */}
+                    <div>
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="#">Audio</a> 
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="#">Live TV</a> 
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="#"><FaSearch/></a> 
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="#"><FaUser/></a> 
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 };
 
