266891a5727197135993b90ae1e446659e4d0ac2
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-22 01:45:43 +0600

    item marged

--- src/pages/shared/bottomNav/BottomNav.jsx
+++ src/pages/shared/bottomNav/BottomNav.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import logo from '../../../assets/logo.png'
 import { Link } from 'react-router-dom';
 import './BottomNav.css';
-import { FaBeer, FaSearch, FaUser } from 'react-icons/fa';
+import { FaSearch, FaUser } from 'react-icons/fa';
 
 const BottomNav = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-dark">
+        <nav class="navbar navbar-expand-lg bg-black">
             <div class="container-fluid">
+                {/* logo left side */}
                 <Link class="navbar-brand" href="#"><img src={logo} alt="logo" className='logo' /></Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse " id="navbarTogglerDemo02">
+                    {/* Left side menu items */}
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 fw-semibold text-white">
                         <li class="nav-item">
                             <a class="nav-link text-white active" aria-current="page" href="#">US</a>
