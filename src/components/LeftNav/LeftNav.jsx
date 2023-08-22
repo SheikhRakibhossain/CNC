86539f3e722e6daa892dd90a538b9b1fb2c54da7
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-22 15:34:58 +0600

    categories data has showing with backend connection

--- src/components/LeftNav/LeftNav.jsx
+++ src/components/LeftNav/LeftNav.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftNav = () => {
+
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+
+    }, [])
     return (
         <div>
-           <h3>I am left nav</h3> 
+            <h3>I am left nav</h3>
         </div>
     );
 };
--- src/pages/shared/bottomNav/BottomNav.jsx
+++ src/pages/shared/bottomNav/BottomNav.jsx
@@ -6,73 +6,73 @@ import { FaSearch, FaUser } from 'react-icons/fa';
 
 const BottomNav = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-black">
-            <div class="container-fluid">
+        <nav className="navbar navbar-expand-lg bg-black">
+            <div className="container-fluid">
                 {/* logo left side */}
-                <Link class="navbar-brand" href="#"><img src={logo} alt="logo" className='logo' /></Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <Link className="navbar-brand" href="#"><img src={logo} alt="logo" className='logo' /></Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse " id="navbarTogglerDemo02">
+                <div className="collapse navbar-collapse " id="navbarTogglerDemo02">
                     {/* Left side menu items */}
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 fw-semibold text-white">
-                        <li class="nav-item">
-                            <a class="nav-link text-white active" aria-current="page" href="#">US</a>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 fw-semibold text-white">
+                        <li className="nav-item">
+                            <a className="nav-link text-white active" aria-current="page" href="#">US</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="#">World</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" href="#">World</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white " aria-disabled="true">Politics</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white " aria-disabled="true">Politics</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" aria-disabled="true">Business</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" aria-disabled="true">Business</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" aria-disabled="true">Opinions</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" aria-disabled="true">Opinions</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white " aria-disabled="true">Health</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white " aria-disabled="true">Health</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" aria-disabled="true">Entertainment</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" aria-disabled="true">Entertainment</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" aria-disabled="true">Style</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" aria-disabled="true">Style</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" aria-disabled="true">Travel</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" aria-disabled="true">Travel</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" aria-disabled="true">Sports</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" aria-disabled="true">Sports</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" aria-disabled="true">Video</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" aria-disabled="true">Video</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" aria-disabled="true">Research</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" aria-disabled="true">Research</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" aria-disabled="true">Education</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" aria-disabled="true">Education</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" aria-disabled="true">Foods</a>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" aria-disabled="true">Foods</a>
                         </li>
                     </ul>
                     {/* Right nav bar */}
                     <div>
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                            <a class="nav-link active text-white" aria-current="page" href="#">Audio</a> 
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                            <a className="nav-link active text-white" aria-current="page" href="#">Audio</a> 
                             </li>
-                            <li class="nav-item">
-                            <a class="nav-link active text-white" aria-current="page" href="#">Live TV</a> 
+                            <li className="nav-item">
+                            <a className="nav-link active text-white" aria-current="page" href="#">Live TV</a> 
                             </li>
-                            <li class="nav-item">
-                            <a class="nav-link active text-white" aria-current="page" href="#"><FaSearch/></a> 
+                            <li className="nav-item">
+                            <a className="nav-link active text-white" aria-current="page" href="#"><FaSearch/></a> 
                             </li>
-                            <li class="nav-item">
-                            <a class="nav-link active text-white" aria-current="page" href="#"><FaUser/></a> 
+                            <li className="nav-item">
+                            <a className="nav-link active text-white" aria-current="page" href="#"><FaUser/></a> 
                             </li>
                         </ul>
                     </div>
