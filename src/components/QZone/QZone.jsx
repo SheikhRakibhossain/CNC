a8c6022fe187b8e886a0ac63c185058e906c2a52
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-22 13:49:58 +0600

    issue fix for left side bar

--- src/components/QZone/QZone.jsx
+++ src/components/QZone/QZone.jsx
@@ -6,9 +6,9 @@ import Qzone3 from '../../assets/qZone3.png';
 const QZone = () => {
     return (
         <div gap={6} className='bg-black text-center py-5 px-4'>
-            <img className='my-5' src={Qzone1} alt="picture" />
-            <img className='my-5' src={Qzone2} alt="picture" />
-            <img className='my-5' src={Qzone3} alt="picture" />
+            <img className='my-4' src={Qzone1} alt="picture" />
+            <img className='my-4' src={Qzone2} alt="picture" />
+            <img className='my-4' src={Qzone3} alt="picture" />
         </div>
     );
 };
--- src/components/RightNav/RightNav.jsx
+++ src/components/RightNav/RightNav.jsx
@@ -3,8 +3,9 @@ import QZone from '../QZone/QZone';
 
 const RightNav = () => {
     return (
-        <div className='container mx-auto'>
+        <div className='container mx-auto pt-4'>
             <h3>Show Queen Zone</h3>
+            <hr className='text-danger fw-' />
             <QZone/>
         </div>
     );
