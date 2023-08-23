8ff9de286a4f9f02cca38fce85c1ed8d01571e36
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-23 21:56:32 +0600

    user observer function created on authprovider component

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase.config';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
 
     const createUser = (email, password) =>{
         return createUserWithEmailAndPassword(auth, email, password)
@@ -14,7 +15,19 @@ const AuthProvider = ({children}) => {
     const logIn = (email, password) =>{
         return signInWithEmailAndPassword(auth, email, password)
     }
-    const user = {displayName:"Rakib"}
+    // user observer created
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, loggedUser =>{
+            console.log('Logged user staying on state observer', loggedUser);
+            setUser(loggedUser)
+        })
+        return ()=>{
+            return unsubscribe()
+        }
+
+
+    },[])
+
     const authInfo = {
         user,
         createUser,
