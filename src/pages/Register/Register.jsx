040dda8220942a5b288851d70313a89bdb8fb99b
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-23 21:11:13 +0600

    register form user creation feature added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,49 +1,73 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import { AuthContext } from '../../provider/AuthProvider';
 
 const Register = () => {
+    const {createUser} = useContext(AuthContext);
+    const handleRegister = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photo = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, password, email, photo)
+        createUser(email, password)
+        .then(result=>{
+            const createdUser = result.user;
+            console.log(createdUser)
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+
+    }
     return (
         <>
             <Container className='w-50 mx-auto '>
-                <Form >
+                <Form onSubmit={handleRegister} >
                     <Row className="mb-3">
-                        <Form.Group as={Col} md="12" controlId="validationCustom01">
+                        <Form.Group as={Col} md="12">
                             <Form.Label>Your Name</Form.Label>
                             <Form.Control
                                 required
+                                name="name"
                                 type="text"
                                 placeholder="Write your name"
                                 defaultValue="Rakib"
                             />
                             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                         </Form.Group>
-                        <Form.Group as={Col} md="12" controlId="validationCustom01">
+                        <Form.Group as={Col} md="12" >
                             <Form.Label>Photo URL</Form.Label>
                             <Form.Control
                                 required
+                                name="photoURL"
                                 type="text"
                                 placeholder="Photo Link"
                             />
                             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                         </Form.Group>
-                        <Form.Group as={Col} md="12" controlId="validationCustom01">
+                        <Form.Group as={Col} md="12" >
                             <Form.Label>Email</Form.Label>
                             <Form.Control
                                 required
+                                name="email"
                                 type="email"
                                 placeholder="Write your email"
                                 defaultValue="Rakib212@gmail.com"
                             />
                             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                         </Form.Group>
-                        <Form.Group as={Col} md="12" controlId="validationCustom01">
+                        <Form.Group as={Col} md="12" >
                             <Form.Label>Password</Form.Label>
                             <Form.Control
                                 required
+                                name="password"
                                 type="password"
                                 placeholder="Write your password"
                                 defaultValue="@#aH!#12!_)()"
@@ -55,6 +79,7 @@ const Register = () => {
                     <Form.Group className="mb-3">
                         <Form.Check
                             required
+                            name="terms"
                             label="Agree to our terms and conditions !"
                             feedback="You must agree before submitting."
                             feedbackType="invalid"
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from '../firebase.config';
 
 export const AuthContext = createContext(null);
@@ -8,9 +8,13 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
 
+    const createUser = (email, password) =>{
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
     const user = {displayName:"Rakib"}
     const authInfo = {
         user,
+        createUser,
     };
     return (
         <AuthContext.Provider value={authInfo}>
