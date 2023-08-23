327aa8090aab6a43c2d5ea44cd3338d4243a558d
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-23 21:19:12 +0600

    login html has made with react bootstrap

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
 const Login = () => {
     return (
-        <div>
-            I am login page
-        </div>
+      <>
+       <Container className='w-50 mx-auto '>
+        <h2>Login Please !</h2>
+                <Form>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="12" >
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                required
+                                name="email"
+                                type="email"
+                                placeholder="Write your email"
+                                defaultValue="Rakib212@gmail.com"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} md="12" >
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                required
+                                name="password"
+                                type="password"
+                                placeholder="Write your password"
+                                defaultValue="@#aH!#12!_)()"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                    </Row>
+                    <Button type="submit" className='btn btn-warning border-none btn-sm'>Submit</Button>
+                </Form>
+            </Container>
+      </>
     );
 };
 
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase.config';
 
 export const AuthContext = createContext(null);
@@ -11,10 +11,14 @@ const AuthProvider = ({children}) => {
     const createUser = (email, password) =>{
         return createUserWithEmailAndPassword(auth, email, password)
     }
+    const logIn = (email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
     const user = {displayName:"Rakib"}
     const authInfo = {
         user,
         createUser,
+        logIn,
     };
     return (
         <AuthContext.Provider value={authInfo}>
