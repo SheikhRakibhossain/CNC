57efd6aac3b941ec59df66591291c0ae7895b40f
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-24 22:40:41 +0600

    terms and condition page has been created

---
+++ src/pages/termsAndCondition/Terms.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Terms = () => {
+    return (
+        <div>
+            <h3>Our Terms and condition </h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nemo quaerat cupiditate. Accusantium necessitatibus consequuntur accusamus adipisci nam ipsa enim magnam sint reprehenderit ut cum aperiam voluptates et commodi, totam eaque quasi facere amet, debitis error facilis? Laborum, quaerat eum! Facere autem sint ad expedita in beatae voluptatibus ipsam accusantium pariatur explicabo unde itaque ab labore, inventore error repellat repellendus illo, vel sequi totam adipisci provident necessitatibus? Aspernatur, minus laboriosam doloremque rem in laudantium reprehenderit possimus nisi asperiores ea excepturi dolorum fuga! Libero consequatur fugiat repellendus minus impedit. Quis exercitationem ex laborum, corporis perferendis aperiam saepe delectus sunt, repellat harum recusandae adipisci nemo, natus est. Odit nam ratione temporibus minus distinctio minima explicabo rerum optio quasi officiis natus incidunt quas corporis praesentium, aliquid qui magni ex quam? Mollitia itaque quaerat sunt praesentium assumenda repellendus, accusamus necessitatibus quod nobis aspernatur beatae possimus alias suscipit iste commodi, minus architecto voluptas. Error quae dolorum, in laboriosam molestias adipisci nisi aliquam qui, eveniet aspernatur temporibus quia praesentium magnam iste voluptatum facere consequuntur. Doloremque exercitationem sunt deleniti aliquid laudantium modi nemo necessitatibus optio nesciunt, quod totam, fuga beatae doloribus laboriosam, hic distinctio sint recusandae possimus dolores nam. Voluptatum debitis mollitia quam cumque aperiam necessitatibus rem!</p>
+            <Link to='/register'>Accept out terms and condition when you created your account</Link>
+        </div>
+    );
+};
+
+export default Terms;
