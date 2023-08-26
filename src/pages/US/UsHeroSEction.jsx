c09aad2080b6404ab3179352a91c81fcd74ff6dc
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-26 23:36:44 +0600

    us card section created and hero section make separate

---
+++ src/pages/US/UsHeroSEction.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const UsHeroSEction = () => {
+    return (
+        <section className='usSection '>
+                <div className='bgText pb-6'>
+                    <h2 className='fs-1 fw-bold py-3'>Florida elementary school principal and teacher are placed on leave after Black students are singled out at an assembly</h2>
+                    <p>
+                        The principal and a teacher at a Flagler County, Florida, elementary school are on paid administrative leave after an assembly was held only for fourth and fifth-grade Black students, who were collectively told to improve their school performance, according to the school district – regardless of how each student was doing individually.
+
+                        The two school staffers were placed on leave days after the assembly was held at Bunnell Elementary School on August 18. Only African American students from the two grades participated, Flagler County Schools spokesperson Jason Wheeler told CNN.
+
+                        During the assembly, a 2023-2024 school year Goals and Objectives PowerPoint presentation was used that read in part, “AA have underperform (sic) on standardized assessment for the last past 3 years,” Flagler County School Board chair Cheryl Massaro told CNN in an email.</p>
+                        <Button variant='warning'>Read More...</Button>
+                </div>
+            </section>
+    );
+};
+
+export default UsHeroSEction;
