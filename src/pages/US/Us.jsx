0c7d417c6ec24a6edbdea8f358863151019bf4d5
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-26 23:04:52 +0600

    car display overly make colorful update

--- src/pages/US/Us.jsx
+++ src/pages/US/Us.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Col, Image, Row } from 'react-bootstrap';
+import { Button, Card, Col, Image, Row } from 'react-bootstrap';
 import Container from 'react-bootstrap/esm/Container';
 import aicar from '../../assets/girlscat/aicar.jpg'
 
 const Us = () => {
     return (
         <Container>
-            <Row>
-                <Col>
-                    <Image src={aicar} alt="car" />
-                </Col>
-            </Row>
+            <Card className="bg-dark text-white text-center">
+                <Card.Img src={aicar} alt="car" />
+                <Card.ImgOverlay>
+                    
+                    <Card.Title className='fs-2'>Card title</Card.Title>
+                    <Card.Text className='fs-4'>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                    </Card.Text>
+                    <Button variant='warning'>Let's Share</Button>
+               
+                </Card.ImgOverlay>
+            </Card>
         </Container>
     );
 };
