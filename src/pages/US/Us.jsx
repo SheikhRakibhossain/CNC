db4e195a4952c6110563041f571e73cf6e65346a
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-26 20:35:24 +0600

    us page create and image added

---
+++ src/pages/US/Us.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from 'react-bootstrap/esm/Container';
+
+const Us = () => {
+    return (
+        <Container>
+            <img src="../../assets/girlscat/car for us news.jpg" alt="car" />
+        </Container>
+    );
+};
+
+export default Us;
