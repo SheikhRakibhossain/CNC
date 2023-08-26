83412d97ea7c10199a14cc5b4af07432e6dc823d
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-26 20:42:57 +0600

    car image added

--- src/pages/US/Us.jsx
+++ src/pages/US/Us.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Col, Image, Row } from 'react-bootstrap';
 import Container from 'react-bootstrap/esm/Container';
+import aicar from '../../assets/girlscat/aicar.jpg'
 
 const Us = () => {
     return (
         <Container>
-            <img src="../../assets/girlscat/car for us news.jpg" alt="car" />
+            <Row>
+                <Col>
+                    <Image src={aicar} alt="car" />
+                </Col>
+            </Row>
         </Container>
     );
 };
