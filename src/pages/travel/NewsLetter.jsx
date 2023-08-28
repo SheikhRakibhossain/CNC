1f034a75e1177b1ffaa41b5a1c35f38c0285d01d
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-29 00:02:57 +0600

    Button style added

--- src/pages/travel/NewsLetter.jsx
+++ src/pages/travel/NewsLetter.jsx
@@ -4,12 +4,12 @@ const NewsLetter = () => {
     return (
         <>
             <section className=' container bg-warning py-5'>
-                <h1 className='text-center'>Stay Tune With Us</h1>
+                <h1 className='text-center'>Stay Tune With Us !!!</h1>
                 <p className='text-center text-danger fst-italic'>Know our latest news and update. Write your email and get a membership from us to continue your vision using CNC.</p>
 
                 <form className='d-flex justify-content-center align-items-center '>
                     <input type="email" placeholder='Write email' class="form-control py-2 w-75" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <button type="submit" class="btn btn-outline-success btn-sm" style={{ padding: '9px 3px' }}>Subscribe</button>
+                    <button type="submit" class="btn btn-outline-success btn-sm" style={{ padding: '9px 30px' }}>Subscribe</button>
 
                 </form>
 
