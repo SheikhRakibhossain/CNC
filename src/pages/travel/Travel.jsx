09823470dd60604d32235932ba8d7cc631cad259
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-28 14:27:56 +0600

    travel page 3rd section map component created

--- src/pages/travel/Travel.jsx
+++ src/pages/travel/Travel.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TravelHero from './TravelHero';
 import TravelCard from './TravelCard';
+import TravelMap from './TravelMap';
 
 const Travel = () => {
     return (
         <>
             <TravelHero/>
             <TravelCard/>
+            <TravelMap/>
         </>
     );
 };
---
+++ src/pages/travel/TravelMap.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TravelMap = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TravelMap;
