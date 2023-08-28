638e03786de055f7de7fc62bbb1dab2f59889ff6
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-28 17:23:09 +0600

    map component creation task on going

--- src/pages/travel/TravelMap.jsx
+++ src/pages/travel/TravelMap.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
 
 const TravelMap = () => {
+    const defaultProps = {
+        center: {
+          lat: 10.99835602,
+          lng: 77.01502627
+        },
+        zoom: 11
+      };
     return (
         <>
-        <section>
-            <h2 className='text-center fs-3 text-uppercase fst-italic'>Let's Choose your dream destination</h2>
-            <hr class="border border-danger border-2 "></hr>
-        </section>
-            
+            <section>
+                <div>
+                    <h2 className='text-center fs-3 text-uppercase fst-italic'>Let's Choose your dream destination</h2>
+                    <hr className="border border-danger border-2 "></hr>
+
+                </div>
+                <div>
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: "3cba09ec9a157c51" }}
+                        defaultCenter={defaultProps.center}
+                        defaultZoom={defaultProps.zoom}
+                    >
+                        <AnyReactComponent
+                            lat={59.955413}
+                            lng={30.337844}
+                            text="My Marker"
+                        />
+                    </GoogleMapReact>
+                </div>
+            </section>
+
+
         </>
     );
 };
