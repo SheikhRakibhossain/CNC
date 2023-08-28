aac49a27b371620deea9ad2c5877162417b8f97f
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-28 14:35:08 +0600

    map componanet title added

--- src/pages/travel/TravelMap.jsx
+++ src/pages/travel/TravelMap.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const TravelMap = () => {
     return (
-        <div>
+        <>
+        <section>
+            <h2 className='text-center fs-3 text-uppercase fst-italic'>Let's Choose your dream destination</h2>
+            <hr class="border border-danger border-2 "></hr>
+        </section>
             
-        </div>
+        </>
     );
 };
 
