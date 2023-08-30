074db46aaa2ee926c21a4b47112411436bcc78b9
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-31 01:13:32 +0600

    Rest of the context have added

--- src/pages/StylePage/Arts.jsx
+++ src/pages/StylePage/Arts.jsx
@@ -25,10 +25,20 @@ const Arts = () => {
                     <Col lg={5}>
                         <img src={vote} alt="museum" />
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, deleniti at maxime possimus rerum est saepe temporibus vero amet nihil corrupti quo ratione perferendis sapiente aperiam, suscipit asperiores accusamus. Nam!</p>
+                        <hr className="border opacity-50" />
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo non quia eius repudiandae maxime quidem!</p>
+                        <hr className="border opacity-50 " />
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo non quia eius repudiandae maxime quidem!</p>
                     </Col>
                     <Col lg={2}>
                         <img src={singer} alt="museum" />
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat excepturi tempore deleniti molestiae est in mollitia commodi? Autem placeat, adipisci magnam soluta perspiciatis facere sed nesciunt praesentium? Officiis voluptatum eligendi praesentium provident asperiores ullam sit.</p>
+                        <hr className="border opacity-50" />
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <hr className="border opacity-50 " />
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <hr className="border opacity-50" />
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
                     </Col>
 
                 </Row>
