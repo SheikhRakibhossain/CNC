2f902561fefe6e2df60939458f0e66153f7b4d33
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-30 20:08:03 +0600

    card image and hover effect added

--- src/pages/StylePage/LatestStyle.jsx
+++ src/pages/StylePage/LatestStyle.jsx
@@ -3,32 +3,48 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from "react-router-dom";
-import latestImage1 from '../../assets/girlscat/latestMain.jpg';
-import latestMainImage from '../../assets/girlscat/latest-1.jpg';
+import latestMainImage from '../../assets/girlscat/latestMain.jpg';
+import latestImage1 from '../../assets/girlscat/latest-11.jpg';
 import latestImage2 from '../../assets/girlscat/latest-2.jpg';
 import latestImage3 from '../../assets/girlscat/latest-3.jpg';
-import latestImage4 from '../../assets/girlscat/latest-4.jpg';
+import latestImage4 from '../../assets/girlscat/latestImage-4.jpg';
 import './LatestStyle.css';
 
 const LatestStyle = () => {
     return (
         <>
-            <Container className='py-4'>
+            <Container className='py-4 container-fluid'>
                 <Row>
                     <Col lg={6} xs={12} className='latestCard '>
                         <Link to='/' >
-                            <img src={latestMainImage} alt="" />
+                            <img src={latestMainImage} alt="card Image" />
                             <h3 className='text-uppercase fst-italic py-4 fw-light link-offset-3-hover'>Why this photographer wants to see more women like her behind the lens</h3>
                         </Link>
-                    
+
                     </Col>
 
                     <Col lg={3} xs={12}>
-                        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, obcaecati.</h1>
+                        <Link to='/' >
+                            <img src={latestImage3} alt="card Image" />
+                            <h3 className='fs-5 text-uppercase fst-normal py-2 fw-bold '>Why this photographer wants to see more women like her behind the lens</h3>
+                        </Link>
+
+                        <Link to='/' >
+                            <img src={latestImage1} alt="card Image" />
+                            <h3 className='fs-5 text-uppercase fst-normal py-2 fw-bold '>Why this photographer wants to see more women like her behind the lens</h3>
+                        </Link>
+                        <Link to='/' >
+                            <img src={latestImage2} alt="card Image" />
+                            <h3 className='fs-5 text-uppercase fst-normal py-2 fw-bold '>Why this photographer wants to see more women like her behind the lens</h3>
+                        </Link>
+
                     </Col>
 
                     <Col lg={3} xs={12}>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis laborum officia nisi quia esse, consequuntur ex dolorem. Ratione, illo autem.</p>
+                        <Link to='/' >
+                            <img src={latestImage4} alt="card Image" />
+                            <h3 className='text-uppercase fst-italic py-4 fw-light link-offset-3-hover'>Why this photographer wants to see more women like her behind the lens</h3>
+                        </Link>
                     </Col>
                 </Row>
 
