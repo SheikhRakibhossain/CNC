92dc24ae8f6fd9382879f3473d1b0024f778624a
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-31 19:40:46 +0600

    fashion section title and card created

--- src/pages/StylePage/Fashion.jsx
+++ src/pages/StylePage/Fashion.jsx
@@ -10,25 +10,27 @@ const Fashion = () => {
 
     return (
         <>
-        <Container>
-            <Row xs={1} md={2} lg={4} className="g-4 py-4">
-                {Array.from({ length: 8 }).map((_, idx) => (
-                    <Col key={idx}>
-                        <Card>
-                            <Card.Img variant="top" src={fashion} />
-                            <Card.Body>
-                                <Card.Title>Card title</Card.Title>
-                                <Card.Text>
-                                    This is a longer card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit
-                                    longer.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
-        </Container>
+            <Container>
+                <h1 className=''>Arts</h1>
+                <hr className="border border-dark border-1 opacity-75" />
+                <Row xs={1} md={2} lg={4} className="g-4 py-4">
+                    {Array.from({ length: 8 }).map((_, idx) => (
+                        <Col key={idx}>
+                            <Card>
+                                <Card.Img variant="top" src={fashion} />
+                                <Card.Body>
+                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This is a longer card with supporting text below as a natural
+                                        lead-in to additional content. This content is a little bit
+                                        longer.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </>
     );
 };
