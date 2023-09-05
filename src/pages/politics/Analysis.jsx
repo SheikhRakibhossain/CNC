c20bbc8014e1c37de297e7a4007e17dbd90157e5
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-05 19:15:03 +0600

    politics page 2nd section has created

---
+++ src/pages/politics/Analysis.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import trumpVote from '../../assets/politics/trum2.jpg';
+import talkShow from '../../assets/politics/talkshow.jpg';
+
+const Analysis = () => {
+    return (
+        <>
+
+            <h1>CNN Analysis</h1>
+            <hr className="border border-dark " />
+            <Row>
+                <Col lg={6}>
+                        <img src={talkShow} alt="talking fight each with other" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque deserunt voluptatibus quod dignissimos enim iure obcaecati. Consequuntur dolores rerum consequatur.</p>
+                </Col>
+                <Col lg={6}>
+                        <img src={talkShow} alt="talking fight each with other" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque deserunt voluptatibus quod dignissimos enim iure obcaecati. Consequuntur dolores rerum consequatur.</p>
+                </Col>
+            </Row>
+
+        </>
+    );
+};
+
+export default Analysis;
--- src/pages/politics/Politics.jsx
+++ src/pages/politics/Politics.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import PHero from './PHero';
+import Analysis from './Analysis';
 
 const Politics = () => {
     return (
         <>
         <Container className='py-5'>
             <PHero/>
+            <Analysis/>
 
 
         </Container>
