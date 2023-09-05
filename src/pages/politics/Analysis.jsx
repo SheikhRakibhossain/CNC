bd8bfe4d65fafc5d6509db30e0738c0189bf22bb
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-05 19:19:50 +0600

    politics page 2nd section error has fixed

--- src/pages/politics/Analysis.jsx
+++ src/pages/politics/Analysis.jsx
@@ -7,16 +7,16 @@ const Analysis = () => {
     return (
         <>
 
-            <h1>CNN Analysis</h1>
+            <h1 className='pt-5'>CNN Analysis</h1>
             <hr className="border border-dark " />
             <Row>
                 <Col lg={6}>
                         <img src={talkShow} alt="talking fight each with other" />
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque deserunt voluptatibus quod dignissimos enim iure obcaecati. Consequuntur dolores rerum consequatur.</p>
+                        <p className='fs-5 fw-bold pt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque deserunt voluptatibus quod dignissimos enim iure obcaecati. Consequuntur dolores rerum consequatur.</p>
                 </Col>
                 <Col lg={6}>
-                        <img src={talkShow} alt="talking fight each with other" />
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque deserunt voluptatibus quod dignissimos enim iure obcaecati. Consequuntur dolores rerum consequatur.</p>
+                        <img src={trumpVote} alt="talking fight each with other" />
+                        <p className='fs-5 fw-bold pt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque deserunt voluptatibus quod dignissimos enim iure obcaecati. Consequuntur dolores rerum consequatur.</p>
                 </Col>
             </Row>
 
